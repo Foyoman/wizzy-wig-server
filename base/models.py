c6f197b0694c564103203c2136a8706908a5ac82
donut <edwardtdo@gmail.com>
2023-09-28 21:22:52 +1000

    file model to fit app

--- base/models.py
+++ base/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 
 class File(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    body = models.TextField()
+    title = models.CharField(default="Untitled", max_length=30)
+    content = models.TextField(null=True, blank=True)
+    date_created = models.DateTimeField(default=timezone.now)
+    last_modified = models.DateTimeField(auto_now=True)
+    is_folder = models.BooleanField(default=False)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    def __str__(self):
+        return self.title
