2c5b85426919be5817165adabf7b5f8f4bbb2bbe
Edward Do <edwardtdo@gmail.com>
2023-10-11 15:49:43 +1100

    returning tokens on user registration

--- base/api/urls.py
+++ base/api/urls.py
@@ -7,12 +7,11 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('users/', views.create_user, name='create-user'),
+    path('users/register/', views.create_user, name='create-user'),
 
     path('', views.get_routes),
     path('files/', views.files_handler, name="files-handler"),
-    path('files/<int:file_id>/', views.file_detail_handler,
-         name="file-detail-handler"),
+    path('files/<int:file_id>/', views.file_detail_handler, name="file-detail-handler"),
 
     path('token/', MyTokenObtainPairView.as_view(), name='token-obtain-pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
--- base/api/views.py
+++ base/api/views.py
@@ -6,6 +6,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .serializers import UserSerializer, FileSerializer
 from base.models import File
@@ -36,8 +37,22 @@ class MyTokenObtainPairView(TokenObtainPairView):
 def create_user(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # create the user 
+        user = serializer.save()
+        
+        # generate jwt tokens for the user
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+        refresh_token = str(refresh)
+        
+        return Response({
+            'access': access_token,
+            'refresh': refresh_token,
+            'user_id': user.id,
+            'email': user.email,
+            'username': user.username
+        }, status=status.HTTP_201_CREATED)
+        
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
